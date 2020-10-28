a8312236c4d6a2871254a72519111085c08a5245
adaosowiecka <40485375+adaosowiecka@users.noreply.github.com>
2020-10-28 18:45:13 +0100

    jhfkjhs

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,32 +1,32 @@
 ﻿using System;
 using System.Net.Http;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cw1
 {
     class Program
     {
-        static async System.Threading.Tasks.Task Main(string[] args)
+        static async Task Main(string[] args)
         {
-            var client = new HttpClient();
-            HttpResponseMessage result = await client.GetAsync("https://www.pja.edu.pl");
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("http://www.pja.edu.pl");
 
-            if(result.IsSuccessStatusCode)
+            if (response.IsSuccessStatusCode)
             {
-                string html = await result.Content.ReadAsStringAsync();
+                var html = await response.Content.ReadAsStringAsync();
                 var regex = new Regex("[a-z0-9]+@+[a-z0-9]+\\.[a-z]+",
                     RegexOptions.IgnoreCase);
 
-                MatchCollection matches = regex.Matches(html);
-                foreach (var m in matches)
+                var matches = regex.Matches(html);
+                foreach(var i in matches)
                 {
-                    Console.WriteLine(m);
+                    Console.WriteLine(i);
                 }
             }
 
-
-            Console.WriteLine("Koniec");
-
+            Console.Write("Koniec!");
+      
         }
     }
 }
